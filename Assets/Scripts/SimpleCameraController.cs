ec1eecf4498a60867535e0e025aaab3566b8bc3f
Midrax <rexridam@gmail.com>
2020-02-13 00:07:16 +0000

    Camera rotation using mouse

--- Assets/Scripts/SimpleCameraController.cs
+++ Assets/Scripts/SimpleCameraController.cs
@@ -8,13 +8,30 @@ public class SimpleCameraController : MonoBehaviour
     public Transform target;
     // The distance to keep
     public float distance = 4.0f;
+
+    // camera rotation script
+    private const float Y_ANGLE_MIN = 0.0f;
+    private const float Y_ANGLE_MAX = 50.0f;
+
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+    private float speedX = 4.0f;
+    private float speedY = 1.0f;
+
     void Update()
     {
-        // Move camera to XZ-plane of target
-        transform.position = new Vector3(transform.position.x, target.position.y, transform.position.z);
-        // Adjust distance from player while preserving camera look angle
-        transform.position = target.position + (transform.position - target.position).normalized * distance;
-        // Face player
-        transform.LookAt(target);
+        currentX += Input.GetAxis("Mouse X");
+        currentY += Input.GetAxis("Mouse Y");
+
+        currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 dir = new Vector3(0,0,-distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        transform.position = target.position + rotation * dir;
+        transform.LookAt(target.position);
+
     }
 }
