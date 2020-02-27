c9448f78e123a5716c00f37efc24a11d5e8389ee
lewdream <lewis@lewiscarts.com>
2020-02-27 17:18:51 +0000

    new

---
+++ Onirico-develop/Assets/Scripts/GlobalVariables.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalVariables : MonoBehaviour
+{
+    public Vector3 playerPosition = new Vector3(1000f, 1000f, 1000f);         // The last global sighting of the player.
+    public Vector3 resetPlayerPosition = new Vector3(1000f, 1000f, 1000f);    // The default position if the player is not in sight.
+
+    public float moveSpeed = 0f;
+    public Text interactionText;
+
+    public void changeText(string s)
+    {
+        interactionText.text = s;
+    }
+}
